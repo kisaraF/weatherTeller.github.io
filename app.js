bed39f5ef4dd51040097c3757b65ccd87e2e9935
kisaraF <73217189+kisaraF@users.noreply.github.com>
2021-01-19 08:42:03 +0530

    Finished inital functions of the app. Only left with styling

---
+++ app.js
@@ -0,0 +1,57 @@
+//Selecting Elements
+let input = document.getElementById('input');
+const btn = document.getElementById('button');
+const display = document.getElementById('display');
+
+//Api Key
+const key = "57e8f06a67f78bf4aa8d54589b7f750a";
+
+input.addEventListener('keyup' , keyEvent);
+btn.addEventListener('click' , doThis);
+
+function keyEvent(e) {
+	if(e.keyCode === 13){
+		doThis();
+	}
+}
+
+function doThis() {
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${key}&units=metric`).then((res) => {
+		return res.json()
+	}).then((data) => {
+		console.log(data);
+		const country = data.sys.country;
+		const desc = data.weather[0].main;
+		const subDesc= data.weather[0].description;
+		const icon = data.weather[0].icon;
+		const cityName= data.name;
+		const temp = data.main.temp;
+		const humidity = data.main.humidity;
+		const pressure = data.main.pressure;
+		const sunrise = data.sys.sunrise;
+		const sunset = data.sys.sunset;
+		const windSpeed = data.wind.speed;
+		const windDirection = data.wind.deg;
+
+		display.innerHTML = `
+			<div class='body'>
+			<img src='http://openweathermap.org/img/wn/${icon}@2x.png'>
+				<ul>
+					<li class='list-item'>City : ${cityName}</li>
+					<li class='list-item'>Country : ${country}</li>
+					<li class='list-item'>Condition : ${desc} , ${subDesc}</li>
+					<li class='list-item'>Temperature : ${temp}<span>&#8451;</span></li>
+					<li class='list-item'>Wind Speed : ${windSpeed}<span>meter/sec</span></li>
+					<li class='list-item'>Wind Direction : ${windDirection}<span>&#176;</span></li>
+					<li class='list-item'>Humidity : ${humidity}% </li>
+					<li class='list-item'>Pressure : ${pressure}hPa </li>
+					<li class='list-item'>Sunrise : ${sunrise} </li>
+					<li class='list-item'>Sunset : ${sunset} </li>
+				</ul>
+			</div>
+		`
+
+	}).catch((err) => {
+		console.log('Wrong Typing Son')
+	})
+}
